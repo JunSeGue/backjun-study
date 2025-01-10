d4194b9e320743fcb610cfa08b8277b6a8c7017f
전세계 <kssjssk@gmail.com>
2025-01-10 09:27:46 +0900

    [level 1] Title: 실패율, Time: 5395.54 ms, Memory: 67 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/42889. 실패율/실패율.js
+++ JavaScript/프로그래머스/1/42889. 실패율/실패율.js
@@ -1,17 +1,12 @@
 //n = 전체 스테이지 수
 function solution(n, stages) {
-    let answer = new Map();
-    let fail = 0;
-    let challenger = stages.length;
+    let result = [];
     // 실패율 = 클리어X / 클리어
     for (let i=1; i <= n; i++) {
-        let clear = stages.filter(stage => stage > i).length;
-        challenger = challenger - fail;
-        fail = challenger - clear;
-        let failRate = fail/challenger;
-        answer.set(i, failRate);
+        let clear = stages.filter(stage => stage >= i).length;
+        let fail = stages.filter(stage => stage === i).length;
+        result.push([i,fail/clear]);
     }
-    let sortedArray = Array.from(answer).sort((a,b) => b[1]-a[1]) 
-    let sortedKeys = sortedArray.map(item => item[0]);
-    return sortedKeys;
+     result.sort((a,b) => b[1] - a[1]);
+    return result.map((x) => x[0]);
 }
