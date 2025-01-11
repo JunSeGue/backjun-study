1b9bcab974c0a2bd66386a6d178f21841c4c69ef
전세계 <kssjssk@gmail.com>
2025-01-11 23:28:20 +0900

    [level 2] Title: H-Index, Time: 0.63 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/42747. H－Index/H－Index.js
+++ JavaScript/프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,10 +1,29 @@
 function solution(citations) {
-    let answer= 0 ;
+    let hIndex= 0 ;
     let sortedArray = citations.sort((a,b) => b-a);
+    
     for(let i=0; i<sortedArray.length; i++){
         if(sortedArray[i] >= i+1){
-           answer = i+1;
+           hIndex = i+1;
+        } else {
+            break;
         }
     }
-    return answer;
+    return hIndex;
+}
+
+//refator code
+function solution(citations) {
+    citations.sort((a,b) => b - a );
+    let hIndex = 0;
+    
+    citations.some((citation, index) => {
+        if(citation >= index +1) {
+            hIndex = hIndex +1;
+            return false;
+        } else {
+            return true;
+        }
+    });
+    return hIndex;
 }
