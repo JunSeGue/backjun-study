9c31b5a54525845f74ff90d107901115d8827805
전세계 <kssjssk@gmail.com>
2025-01-12 18:04:55 +0900

    [level 1] Title: 모의고사, Time: 1.96 ms, Memory: 35.8 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/42840. 모의고사/모의고사.js
+++ JavaScript/프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,24 +1,46 @@
+// function solution(answers) {
+    
+//     let first = [ 1, 2, 3, 4, 5 ];
+//     let second = [ 2, 1, 2, 3, 2, 4, 2, 5 ];
+//     let third = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 ];
+    
+//     let scores = [0,0,0]
+        
+//     answers.forEach((answer,index)=> {
+        
+//         if(answer === first[index % first.length]) scores[0]++;
+//         if(answer === second[index % second.length]) scores[1]++;
+//         if(answer === third [index % third.length]) scores[2]++;
+//     });
+    
+//     const maxScore = Math.max(...scores);
+//     let result = [];
+//     scores.forEach((score,index)=> {
+//         if(score === maxScore) result.push(index+1)
+//     })
+//     return result;
+// }
+
+// code refactoring
 function solution(answers) {
-    
-    let first = [ 1, 2, 3, 4, 5 ];
-    let second = [ 2, 1, 2, 3, 2, 4, 2, 5 ];
-    let third = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 ];
-    
-    let scores = [0,0,0]
-        
-    answers.forEach((answer,index)=> {
-        
-        if(answer === first[index % first.length]) scores[0]++;
-        if(answer === second[index % second.length]) scores[1]++;
-        if(answer === third [index % third.length]) scores[2]++;
-    });
+    const patterns = [
+        [1, 2, 3, 4, 5],                    // 1번 수포자의 패턴
+        [2, 1, 2, 3, 2, 4, 2, 5],           // 2번 수포자의 패턴
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]      // 3번 수포자의 패턴
+    ];
+    let scores = patterns.map(pattern => 
+        // answers.filter((answer,index) => 
+        //     answer === pattern[index % pattern.length]).length);
+        answers.reduce((score, answer, index) =>
+            score + (answer === pattern[index % pattern.length]?1:0),0
+        )
+    );
     
     const maxScore = Math.max(...scores);
-    let result = [];
-    scores.forEach((score,index)=> {
-        if(score === maxScore) result.push(index+1)
-    })
-    return result;
     
+    // return scores
+    //     .map((score, index) => (score === maxScore ? index+1 : null))
+    //     .filter(index => index !== null));
     
+    return scores.flatMap((score, index)=> score === maxScore ? index+1 : []);
 }
