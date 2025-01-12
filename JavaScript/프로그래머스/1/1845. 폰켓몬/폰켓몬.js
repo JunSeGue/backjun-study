aaf64ca8c795ae68642021017438307bd79da5fd
전세계 <kssjssk@gmail.com>
2025-01-12 21:17:41 +0900

    [level 1] Title: 폰켓몬, Time: 0.60 ms, Memory: 34.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ JavaScript/프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,3 +1,4 @@
+/*
 function solution(nums) {
     // maxValue === nums/2;
     // 가장 많이 가져갈 수 있는 종류의 수 (경우의 수 아님)
@@ -6,5 +7,15 @@ function solution(nums) {
     for(let num of nums) {
         set.add(num);
     }
+    return set.size >= max ? max: set.size;
+}
+*/
+
+// refacotriong code
+
+function solution(nums) {
+    let set = new Set(nums);
+    let max = nums.length/2;
+
     return set.size >= max ? max: set.size;
 }
