b3ea2200e753ef5558798461804daf6f3ded6076
전세계 <kssjssk@gmail.com>
2025-01-15 22:28:33 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 1.09 ms, Memory: 33.7 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ JavaScript/프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,26 +1,16 @@
 function solution(survey, choices) {
     const scores = {
-        'R': 0, 'T': 0, 'C': 0, 'F': 0,
-        'J': 0, 'M': 0, 'A': 0, 'N': 0
+        'R': 0, 'T': 0, 'C': 0, 'F': 0,'J': 0, 'M': 0, 'A': 0, 'N': 0
     };
     
     const scoreMap = [3, 2, 1, 0, 1, 2, 3];
    
-    survey.forEach((s, idx) => {
-        const [disagreeType, agreeType] = s.split('');
+    survey.forEach((pair, idx) => {
+        const [disagreeType, agreeType] = pair.split('');
         const choice = choices[idx];
-        if (choice < 4) {
-            scores[disagreeType] += scoreMap[choice - 1];
-        } else if (choice > 4) {
-            scores[agreeType] += scoreMap[choice - 1]; 
-        } 
+        scores[choice < 4 ? disagreeType : agreeType] += scoreMap[choice-1];
     });
     
-    const result = [];
-    result.push(scores['R'] >= scores['T'] ? 'R' : 'T');
-    result.push(scores['C'] >= scores['F'] ? 'C' : 'F');
-    result.push(scores['J'] >= scores['M'] ? 'J' : 'M');
-    result.push(scores['A'] >= scores['N'] ? 'A' : 'N');
-   
-    return result.join('');
-}
+    return ['RT', 'CF','JM', 'AN'].map(pair => 
+        scores[pair[0]] >= scores[pair[1]] ? pair[0] : pair[1]).join('');
+    }
