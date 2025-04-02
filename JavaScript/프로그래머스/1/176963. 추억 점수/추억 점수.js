bcfd4abb3f8a559613be142a39b47d5394abb53c
전세계 <kssjssk@gmail.com>
2025-04-02 15:26:53 +0900

    [level 1] Title: 추억 점수, Time: 2.88 ms, Memory: 35.9 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ JavaScript/프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,22 +1,16 @@
 function solution(name, yearning, photos) {
-    const resultMap = new Map();
-    const answers = [];
+    const map = new Map();
     
-    for(let i = 0 ; i< name.length; i++) {
-        resultMap.set(name[i], yearning[i]);
+  for(let i=0; i<name.length; i++){
+        map.set(name[i], yearning[i])
     }
     
-    for(const photo of photos) {
-        let answer = 0;
-        const keys = resultMap.keys();
-        for(const key of keys) {
-            const findPeople = photo.find(name => name === key);
-            const result = resultMap.get(findPeople);
-            if (result !== undefined) {
-                answer += result;
-        }
-    }
-        answers.push(answer);
-    }
-    return answers
+    return photos.map((arr)=> {
+        let sum = 0;
+        arr.forEach((v) => {
+            sum += map.get(v)?map.get(v) : 0
+        })
+        return sum;
+    });
+      
 }
