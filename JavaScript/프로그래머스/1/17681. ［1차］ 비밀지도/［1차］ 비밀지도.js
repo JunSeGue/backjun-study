9930e9e6355e41f027f99b1b80ba965fe345d98c
전세계 <kssjssk@gmail.com>
2025-04-06 22:57:34 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ JavaScript/프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,11 +1,21 @@
-function solution(n, arr1, arr2) {
-    let answer = [];
+// function solution(n, arr1, arr2) {
+//     let answer = [];
        
-    for (let i = 0; i<n; i++) {
-        const binaryArr = (arr1[i] | arr2[i]).toString(2).padStart(n, "0");
-        const str = binaryArr.replace(/[1-9]/g, "#").replace(/[0]/g, " ");
-        answer.push(str)
+//     for (let i = 0; i<n; i++) {
+//         const binaryArr = (arr1[i] | arr2[i]).toString(2).padStart(n, "0");
+//         const str = binaryArr.replace(/[1-9]/g, "#").replace(/[0]/g, " ");
+//         answer.push(str)
         
-    }
-    return answer;
-}
+//     }
+//     return answer;
+// }
+
+const solution = (n, arr1, arr2) => 
+    
+   arr1.map((num, i) => {
+        return (num | arr2[i])
+            .toString(2)
+            .padStart(n, "0")
+            .replace(/[01]/g, d => d === "1" ? "#" : " ")
+    })
+
