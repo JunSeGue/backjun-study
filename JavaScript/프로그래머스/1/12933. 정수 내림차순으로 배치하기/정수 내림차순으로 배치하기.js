e9b31b2c9f6eb6b0f0313cafaabccded9cea967f
전세계 <kssjssk@gmail.com>
2025-04-10 21:10:52 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
+++ JavaScript/프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -1,6 +1,10 @@
-function solution(n) {
+// function solution(n) {
     
-    let answer = n.toString().split('').map(Number);
-    return Number(answer.sort((a, b) => b-a).join(''));
+//     let answer = n.toString().split('').map(Number);
+//     return Number(answer.sort((a, b) => b-a).join(''));
+ 
+// }
 
+function solution(n) {
+    return Number([...n.toString()].sort((a, b) => b - a).join(''))
 }
