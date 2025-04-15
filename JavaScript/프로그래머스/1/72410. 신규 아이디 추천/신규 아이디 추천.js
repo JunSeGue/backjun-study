b127fcbf6b084dc4fde431e1992fd4d5ee83ab3e
전세계 <kssjssk@gmail.com>
2025-04-15 13:15:53 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.36 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ JavaScript/프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,22 +1,47 @@
-function solution(new_id) {
-    var answer = '';
-    const newId = new_id
-    // 검사 과정
-    // 1단계 소문자 치환
-    const lowerId = newId.toString().toLowerCase();
-    // 2단계 소문자, 숫자, 빼기, 밑줄, 마침표를 제외한 문자 제거
-    const cleanString = lowerId.replace(/[^a-z0-9\-_\.]/g, '');
-    // 3단계 4단계  마침표 2개이상 연속 시 하나로 통일
-    answer = cleanString.split('.').filter(Boolean).join('.');
-    // 5단계 new_id가 빈문자열일 경우에 a로 치환
-    if(answer.trim() === "")
-        answer = 'a';
-    if(answer.length > 15)
-        answer = answer.slice(0,15);
-    if(answer.endsWith('.'))
-        answer = answer.slice(0, -1);
-    while(answer.length < 3)
-        answer += answer[answer.length -1];
+// function solution(new_id) {
+//     var answer = '';
+//     const newId = new_id
+//     // 검사 과정
+//     // 1단계 소문자 치환
+//     const lowerId = newId.toString().toLowerCase();
+//     // 2단계 소문자, 숫자, 빼기, 밑줄, 마침표를 제외한 문자 제거
+//     const cleanString = lowerId.replace(/[^a-z0-9\-_\.]/g, '');
+//     // 3단계 4단계  마침표 2개이상 연속 시 하나로 통일
+//     answer = cleanString.split('.').filter(Boolean).join('.');
+//     // 5단계 new_id가 빈문자열일 경우에 a로 치환
+//     if(answer.trim() === "")
+//         answer = 'a';
+//     if(answer.length > 15)
+//         answer = answer.slice(0,15);
+//     if(answer.endsWith('.'))
+//         answer = answer.slice(0, -1);
+//     while(answer.length < 3)
+//         answer += answer[answer.length -1];
+    
+//     return answer;
+// }
+
+// 리팩토링 코드
+
+const solution = (new_id) => {
+    let answer = new_id
+    // level 1
+    .toLowerCase()
+    
+    .replace(/[^\w\d-_.]/g, '')
+    
+    .replace(/\.{2,}/g, '.')
+    
+    .replace(/^\.|\.$/g, '')
+    
+    .padEnd(1, 'a')
+    
+    .slice(0,15)
+    
+    .replace(/^\.|\.$/g, '')  
+    
+    answer = answer.padEnd(3, answer[answer.length-1]);
     
     return answer;
+    
 }
