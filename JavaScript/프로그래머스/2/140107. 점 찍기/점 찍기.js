f6f3ccff82b4561483596c9b082fea149f4f9b9f
전세계 <kssjssk@gmail.com>
2025-04-15 14:30:58 +0900

    [level 2] Title: 점 찍기, Time: 10.29 ms, Memory: 37.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/140107. 점 찍기/점 찍기.js
+++ JavaScript/프로그래머스/2/140107. 점 찍기/점 찍기.js
@@ -1,7 +1,10 @@
 function solution(k, d) {
+    // k는 기울기 , d 는 distance의 최댓값
     let count = 0;
     for(let i =0; i<=d; i+=k){
+        // 해당 Y좌표에서 가질 수 있는 최대 X값의 좌표
         let maxX= Math.floor(Math.sqrt(d**2 - i**2));
+        // 원점을 포함한 각 좌표에 조건에 만족시 카운트 업
         count += Math.floor(maxX / k) + 1;
     }
     return count;
