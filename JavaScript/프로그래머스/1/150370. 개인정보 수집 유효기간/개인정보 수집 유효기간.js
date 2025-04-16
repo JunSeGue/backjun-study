c88fdb1a831c6adf80ce5f4529bd9c1b3454e69e
전세계 <kssjssk@gmail.com>
2025-04-16 11:18:35 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.67 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ JavaScript/프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,30 +1,31 @@
 function solution(today, terms, privacies) {
-    const todayInt = today.split('.').map(Number);
+    const [todayYear, todayMonth, todayDay] = today.split('.').map(Number);
 
     // reduce()를 사용해 terms를 객체로 변환
-    const termMap = terms.reduce((acc, term) => {
-        const [key, value] = term.split(' ');
-        acc[key] = Number(value);
-        return acc;
-    }, {});
+    const termMap = Object.fromEntries(terms.map(term => {
+        const [type, months] = term.split(' ');
+        return [type, Number(months)];
+    }));
+    
+    const isExpired = (dateStr, termType) => {
+        let [year, month , day] = dateStr.split('.').map(Number);
+        
+        month += termMap[termType];
+        
+        year += Math.floor((month -1) / 12);
+        month = month % 12 || 12;
+        
+        return (year < todayYear ||
+               (year === todayYear && month < todayMonth) ||
+               (year === todayYear && month === todayMonth && 
+               day <= todayDay))
+        
+    };
 
     return privacies
         .map((privacy, i) => {
             const [dateStr, termType] = privacy.split(' ');
-            const dateInt = dateStr.split('.').map(Number);
-
-            // 계약 기간 추가
-            dateInt[1] += termMap[termType];
-
-            // 연도 및 월 조정
-            dateInt[0] += Math.floor((dateInt[1] - 1) / 12);
-            dateInt[1] = (dateInt[1] - 1) % 12 + 1;
-
-            return (dateInt[0] < todayInt[0] ||
-                    (dateInt[0] === todayInt[0] && dateInt[1] < todayInt[1]) ||
-                    (dateInt[0] === todayInt[0] && dateInt[1] === todayInt[1] && dateInt[2] <= todayInt[2]))
-                ? i + 1
-                : null;
+           return isExpired(dateStr, termType) ? i + 1 : null;
         })
         .filter(Number); // `null` 값 제거
 }
