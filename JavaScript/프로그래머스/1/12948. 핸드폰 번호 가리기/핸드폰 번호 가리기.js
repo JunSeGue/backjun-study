5a45c2472d7edcf76ed9056e4c72d10ab93d423b
전세계 <kssjssk@gmail.com>
2025-04-17 13:28:03 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ JavaScript/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,4 +1,4 @@
 function solution(phone_number) {
    
-    return "*".repeat(phone_number.length-4) + phone_number.slice(-4);
+    return phone_number.slice(-4).padStart(phone_number.length, "*");
 }
