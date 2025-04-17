f59ebb80efc0f920e39b4475f90eb6dec5aab0ef
전세계 <kssjssk@gmail.com>
2025-04-17 13:40:00 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ JavaScript/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -2,13 +2,9 @@ function solution(num) {
     let number = num
     let count = 0;
     while (number  !== 1 && count < 500){
-        if(number % 2 ===0 ) {
-           number = number /2
-        } else {
-            number = number *3 +1;
-        }
+        number = number % 2 ===0 ? number/2 : number * 3 +1;
         count ++;
     }
     
-    return count === 500 ? -1 : count;
+    return count !== 500 ? count : -1;
 }
