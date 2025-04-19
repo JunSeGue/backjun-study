149ca6a1a69235aa5ace38518117fbe31c5158a6
전세계 <kssjssk@gmail.com>
2025-04-19 15:14:30 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ JavaScript/프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,16 +1,26 @@
+// function solution(left, right) {
+//     let answer = 0;
+//     for(let i=left; i<=right; i++){
+//         let count = 0;
+//         for(let j=1; j*j <= i; j++){
+//             if(i % j === 0) {
+//                 count++;
+//                 if(j !== i/j) count++;
+//             }
+//         }
+        
+//         answer += count % 2 === 0 ? i : -i;
+//     }
+    
+//     return answer;
+// }
+
 function solution(left, right) {
     let answer = 0;
-    for(let i=left; i<=right; i++){
-        let count = 0;
-        for(let j=1; j*j <= i; j++){
-            if(i % j === 0) {
-                count++;
-                if(j !== i/j) count++;
-            }
-        }
-        
-        answer += count % 2 === 0 ? i : -i;
-    }
     
+    for (let i = left; i<= right; i++) {
+        let sqrt = Math.sqrt(i);
+        answer += (sqrt === Math.floor(sqrt)) ? -i : i;
+    }
     return answer;
 }
