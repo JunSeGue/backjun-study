e4c66e8df5b4a5c6bd5b3fefdeff0e01b9c50907
전세계 <kssjssk@gmail.com>
2025-04-26 21:51:55 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ JavaScript/프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,12 +1,5 @@
 function solution(s) {
     return s.split('')
-            .sort((a, b) => {
-                const isUpperA = a.toUpperCase() === a;
-                const isUpperB = b.toUpperCase() === b;
-
-                if (isUpperA && !isUpperB) return 1; 
-                if (!isUpperA && isUpperB) return -1;  
-                return b.localeCompare(a); 
-            })
+            .sort((a, b) => a<b ? 1 : -1)
             .join('');
 }
