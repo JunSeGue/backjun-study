393d9980d397d2bf54dd7acea2aafae2315fe398
전세계 <kssjssk@gmail.com>
2025-05-03 23:42:47 +0900

    [level 2] Title: 타겟 넘버, Time: 701.72 ms, Memory: 186 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ JavaScript/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,25 +1,35 @@
-// dfs
-function solution(numbers, target) {
-    let count =0;
+// // dfs
+// function solution(numbers, target) {
+//     let count =0;
     
-    function dfs (index, sum) {
-        if(index === numbers.length) {
-            if(sum === target) count++
-            return;
-        }
-        dfs(index + 1, sum + numbers[index]);
-        dfs(index + 1, sum - numbers[index]);
-    }
-    dfs(0,0)
-    return count;
-}
+//     function dfs (index, sum) {
+//         if(index === numbers.length) {
+//             if(sum === target) count++
+//             return;
+//         }
+//         dfs(index + 1, sum + numbers[index]);
+//         dfs(index + 1, sum - numbers[index]);
+//     }
+//     dfs(0,0)
+//     return count;
+// }
 
 
 // bfs
-// function solution(numbers, target) {
-//     let count = 0;
-//     const queue = [[0,0]];
-    
-    
-    
-// }
+function solution(numbers, target) {
+    let count = 0;
+    let queue = [[0, 0]]; // [현재 합계, 인덱스]
+
+    for (let i = 0; i < numbers.length; i++) {
+        let newQueue = [];
+
+        for (let [sum, index] of queue) {
+            newQueue.push([sum + numbers[index], index + 1]);
+            newQueue.push([sum - numbers[index], index + 1]);
+        }
+
+        queue = newQueue; // 큐를 갱신하여 크기를 제한
+    }
+
+    return queue.filter(([sum, index]) => index === numbers.length && sum === target).length;
+}
