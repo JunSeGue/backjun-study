afa999ab3597c09b6a30e1b01f80eeddb6fb7718
전세계 <kssjssk@gmail.com>
2025-05-04 00:05:27 +0900

    [level 2] Title: 타겟 넘버, Time: 6.73 ms, Memory: 89.4 MB -BaekjoonHub

--- Java/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ Java/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,25 +1,50 @@
-import java.util.LinkedList;
-import java.util.Queue;
+// import java.util.LinkedList;
+// import java.util.Queue;
 
 
-class Solution {
-    public int solution(int[] numbers, int target) {
-        int count = 0;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0,0});
+// class Solution {
+//     private static class State {
+//         int sum , index;
+
+//         State(int sum , int index) {
+//         this.sum = sum;
+//         this.index = index;
+//         }
+//     }   
+
+//     public int solution(int[] numbers, int target) {
+//         int count = 0;
+//         Queue<State> queue = new LinkedList<>();
+//         queue.add(new State(0, 0));
         
-        while(!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int sum = current[0];
-            int index = current[1];
-            
-            if(index == numbers.length) {
-                if(sum == target) count++;
-                continue;
-            }
-            queue.add(new int[] {sum + numbers[index], index+1});
-            queue.add(new int[] {sum - numbers[index], index+1});
-        }        
-        return count;
+//         while(!queue.isEmpty()) {
+//             State current = queue.poll();
+                        
+//             if(current.index == numbers.length) {
+//                 if(current.sum == target) count++;
+//                 continue;
+//             }
+//             queue.add(new State(current.sum + numbers[current.index], current.index +1));
+//             queue.add(new State(current.sum - numbers[current.index], current.index +1));
+//         }        
+//         return count;
+//     }
+
+// }
+
+
+// dfs
+class Solution {
+    public int solution (int[] numbers, int target) {
+        return dfs(numbers, target, 0, 0);
+    }
+    
+    private int dfs (int[] numbers, int target, int index, int sum){
+        
+        if (index == numbers.length) {
+            return (sum == target) ?  1: 0;
+        }
+        return dfs(numbers, target, index +1, sum + numbers[index]) 
+            + dfs(numbers, target, index +1, sum - numbers[index]);
     }
 }
